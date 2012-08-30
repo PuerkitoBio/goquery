e0a499278500e41f7a76cff351fca1d2dd29e2ec
Martin Angers <martin.n.angers@gmail.com>
2012-08-29 22:01:41 -0400

    finalized list of jQuery methods to implement

--- goquery.go
+++ goquery.go
@@ -2,6 +2,7 @@ package goquery
 
 // TODO : Benchmarks
 // TODO : Add the following methods:
+// - AndSelf()
 // - Closest()
 // - Parents()
 // - Contains() (static function?)
@@ -11,3 +12,25 @@ package goquery
 // - Find() : Complete with Selection object and Node object as selectors
 // - Has()
 // - Html() ?
+// - Index()
+// - Is()
+// - Last()
+// - Length() / Size()
+// - Map()
+// - Next()
+// - NextAll()
+// - NextUntil()
+// - Not()
+// - Parent()
+// - Parents()
+// - ParentsUntil()
+// - Prev()
+// - PrevAll()
+// - PrevUntil()
+// - PushStack() ?
+// - Siblings()
+// - Slice()
+// - Text()
+// - ToArray() Is not implemented, is Selection.Nodes
+// - Unique() ? Or internally only, to remove duplicates and maintain node order?
+// - Val() ?
