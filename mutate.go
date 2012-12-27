fd970ef22c26b25f3c1aa90bacc55ef9f6125403
M@ Dunlap <matthew.dunlap+hrilab@gmail.com>
2012-12-27 01:30:29 -0500

    started work on mutate.go, added SetText

---
+++ mutate.go
@@ -0,0 +1,29 @@
+package goquery
+
+import (
+	"exp/html"
+	esc "html"
+)
+
+// SetText() replaces the children of each selected node with the given text 
+// (properly escaped of course).
+// This is the same behavior as jQuery's .text() function.
+func (this *Selection) SetText(s string) *Selection {
+	for _, n := range this.Nodes {
+		setNodeText(n, esc.EscapeString(s))
+	}
+	return this
+}
+
+// Replace the given node's children with the given string.
+func setNodeText(node *html.Node, s string) {
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		node.RemoveChild(c)
+	}
+	text := &html.Node{
+		Parent: node,
+		Type:   html.TextNode,
+		Data:   s,
+	}
+	node.AppendChild(text)
+}
