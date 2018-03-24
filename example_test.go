dd77530fdb61fc15fc9dfb00dc3067a7f3f95a95
Martin Angers <martin.n.angers@gmail.com>
2018-03-24 12:12:09 -0400

    update examples to use the preferred NewDocumentFromReader approach

--- example_test.go
+++ example_test.go
@@ -3,14 +3,25 @@ package goquery_test
 import (
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 // This example scrapes the reviews shown on the home page of metalsucks.net.
 func Example() {
+	// Request the HTML page.
+	res, err := http.Get("http://metalsucks.net")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
 	// Load the HTML document
-	doc, err := goquery.NewDocument("http://metalsucks.net")
+	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
